ab03b46937ffa3e6d0416fbb6e15cf5b63f89da6
Alex Pavlenko <oleksa.pavlenko@gmail.com>
2020-10-23 00:15:06 +0300

    Print state instead of reply.

--- addons/fur/commands.py
+++ addons/fur/commands.py
@@ -4,5 +4,5 @@ from . import api
 # noinspection PyUnusedLocal
 @api.hook_command(names=('state', 'status'))
 def print_state(**kwargs):
-    api.reply(api.format.state(api.get_state()))
+    api.print(api.format.state(api.get_state()))
     return api.types.Eat.ALL
