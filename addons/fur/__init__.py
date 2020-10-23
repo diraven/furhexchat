fad098efd196d272aa78dd34ed953861eef64f0d
Alex Pavlenko <oleksa.pavlenko@gmail.com>
2020-10-23 09:42:04 +0300

    Clean up commented out code.

--- addons/fur/__init__.py
+++ addons/fur/__init__.py
@@ -15,21 +15,3 @@ if fr_ctx:
 def get_cases_list(message, **kwargs):
     if message == '#fuelrats':
         api.send_message('MechaSqueak[BOT]', '!list')
-
-# def youpart_cb(word, word_eol, userdata, attributes):
-#     print(dir(attributes.time))
-#     return hexchat.EAT_HEXCHAT
-#
-#
-# ctx: 'hexchat.Context' = hexchat.find_context(server='FurBoard')
-# ctx.prnt('asdf')
-#
-# hexchat.hook_print_attrs(utils.Event.PRIVATE_MESSAGE_TO_DIALOG.value,
-#     youpart_cb)
-# attrs->server_time_utc = 1342224702;
-# hexchat_emit_print (ph, attrs, "Channel Message", "John", "Hi there", "@",
-# NULL);
-# hexchat_event_attrs_free (ph, attrs);
-
-# %C18%H<%H$4$3$1%H>%H%O$t$2
-# %C18%H<%H$3$1%H>%H%O$t$2
