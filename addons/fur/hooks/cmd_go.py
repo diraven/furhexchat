21dffcc7250cf51eeb2de7399055d74b848e6460
Alex Pavlenko <oleksa.pavlenko@gmail.com>
2020-10-23 19:43:54 +0300

    Add assign alias to go command.

--- addons/fur/hooks/cmd_go.py
+++ addons/fur/hooks/cmd_go.py
@@ -5,13 +5,13 @@ from .. import api
 
 # noinspection PyUnusedLocal
 @api.hook_print(
-    match_message=re.compile(r'^!go [^\s]+ [^\s]+'),
+    match_message=re.compile(r'^!(?:go|assign) [^\s]+ [^\s]+'),
 )
 def handler(message: str, **kwargs):
     args = message.split()
     case = api.state.find_case(args[1])
     if not case:
-        return
+        api.print(f'Case not found: {args[1]}')
 
     for rat_nick in args[2:]:
         case.put_rat(nick=rat_nick)
