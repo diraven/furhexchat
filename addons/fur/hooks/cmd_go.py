9056ed496271d7adaf9a2a10de41677719b5f594
Alex Pavlenko <oleksa.pavlenko@gmail.com>
2020-10-23 19:46:37 +0300

    Fix command returns.

--- addons/fur/hooks/cmd_go.py
+++ addons/fur/hooks/cmd_go.py
@@ -12,6 +12,7 @@ def handler(message: str, **kwargs):
     case = api.state.find_case(args[1])
     if not case:
         api.print(f'Case not found: {args[1]}')
+        return
 
     for rat_nick in args[2:]:
         case.put_rat(nick=rat_nick)
--- addons/fur/hooks/cmd_unassign.py
+++ addons/fur/hooks/cmd_unassign.py
@@ -11,6 +11,7 @@ def handler(message: str, **kwargs):
     args = message.split()
     case = api.state.find_case(args[1])
     if not case:
+        api.print(f'Case not found: {args[1]}')
         return
 
     for rat_nick in args[2:]:
