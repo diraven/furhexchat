5c9fa080de3628287eb1b109e21d1504e515dba8
Oleksii Pavlenko <oleksa.pavlenko@gmail.com>
2020-11-10 22:24:31 +0200

    Add a couple more alias test to cover language dependent aliases. Also test for default platform usage.

--- addons/fur/tests/test_aliases.py
+++ addons/fur/tests/test_aliases.py
@@ -1,7 +1,15 @@
+import pytest
+
 from .conftest import MockedHexchat
 
 
-def test_go(hexchat: MockedHexchat):
-    hexchat.send_command('pc test')
-
-    hexchat.command.assert_called_once_with('MSG #mockrats !pc test')
+@pytest.mark.parametrize('sent_cmd,actual_cmd', [
+    ('pc client', 'MSG #mockrats !pc client'),
+    ('prep-fr client', 'MSG #mockrats !prep-fr client'),
+    ('pcfr-fr client', 'MSG #mockrats !pcfr-fr client'),
+    ('fr client', 'MSG #mockrats !pcfr client'),
+])
+def test_go(hexchat: MockedHexchat, sent_cmd, actual_cmd):
+    hexchat.send_command(sent_cmd)
+    hexchat.command.assert_called_once_with(actual_cmd)
+    hexchat.command.reset_mock()
